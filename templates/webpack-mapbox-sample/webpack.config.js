218c2ce3bd17a2f41ec4136031156fa8cd9f33dd
Alexei Yagolnikov <alexei.yagolnikov@skyward.io>
2020-04-13 19:37:15 -0700

    fixed template 'run' issues with 'webpack' and 'wepback-mapbox-sample'

--- templates/webpack-mapbox-sample/webpack.config.js
+++ templates/webpack-mapbox-sample/webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
   entry: './src/index.js',
